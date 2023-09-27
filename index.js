b9b907e284e3f937867db6f3bdb40f35de182f90
SharanKumar95 <ssharankumar95@gmail.com>
2023-09-26 23:58:40 -0500

    NodeJS 	- Added router and converted app to router 	- Added Comments in index.js 	- Installed nodemon - reflected in package.json

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
+// import express package - npm install express
 const express = require('express');
+// Initialize the port you will be using for the listening
 const port = 8000;
+
+/* 
+   Initialize app to point to the express constructor 
+   Now app can be used to call the functions in express
+   */
 const app = express();
-app.use(express.json());
+
+/* Below 2 lines are to fix compilation errors */
+app.use(express.json()); // specify the format of the values sent from Postman
 app.use(express.urlencoded({extended:false}));
 
+//Initialize the router
 const router = express.Router();
-app.get('/', function(request, response){
-    response.send('This is the server!!');
-})
 
-app.post('/addEmployee', function(request, response){
+/* app changed to router */
+/* get is used to send the response to the client */
+router.get('/', function(request, response){
+    response.send('This is the server!!');
+});
+
+/* addEmployee is a route */
+router.post('/addEmployee', function(request, response){
     let empName = request.body.empName;
     let empPass = request.body.empPass;
 
-    response.end(This is your name ${empName} and password ${empPass});
+    response.end(`This, is your name ${empName} and password ${empPass}`);
+});
+
+router.get('/aboutUs', function(request, response){
+    response.end('This is the about Us page');
 })
+// Bind the app and the Router: Register the router with the app
+app.use('/', router);
+
+/* Listen to connections on this port */
 app.listen(port, function(){
-    console.log("I'm listening on port: "+ port);
-})
+    console.log("App is listening on port: "+ port);
+});
